da2a86d9424e13f7eae4f5265fb8852da8765103
Ashimriat <37146233+Ashimriat@users.noreply.github.com>
2021-02-11 23:59:57 +0300

    Update bootstrap.js
    Поправил табуляцию

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,8 +3,7 @@ import store from './store';
 import App from "./App";
 
 
-window.APP = new Vue({
-	store,
-	render: h => h(App)
-});
-window.APP.$mount('#app');
+new Vue({
+  store,
+  ender: h => h(App)
+}).$mount('#app');

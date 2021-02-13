4fc4f767e5245c282fa8760193cb0a93d6b259fa
Ashimriat <37146233+Ashimriat@users.noreply.github.com>
2021-02-13 15:25:26 +0300

    Update getters.js
    Поправил отступы

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,7 +1,7 @@
 export default {
-	ROOMS: {
-		ACTIVE_ROOM: 'activeRoom',
-		GET_ROOM_INDEX_BY_NAME: 'getRoomIndexByName',
-		GET_ROOM_INDEX_BY_ID: 'getRoomIndexById'
-	}
+  ROOMS: {
+    ACTIVE_ROOM: 'activeRoom',
+    GET_ROOM_INDEX_BY_NAME: 'getRoomIndexByName',
+    GET_ROOM_INDEX_BY_ID: 'getRoomIndexById'
+  }
 };
